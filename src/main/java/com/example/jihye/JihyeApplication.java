7b48b9615d76c83ac45167606e0ac39f1d1e2152
sehongpark <sehong.park86@gmail.com>
2020-09-30 04:14:07 +0900

    Initial commit

---
+++ src/main/java/com/example/jihye/JihyeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jihye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JihyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jihye/JihyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jihye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
