23d67be770e3229e8765251bdb9a238381b35fe8
sehongpark <sehong.park86@gmail.com>
2020-09-30 06:03:03 +0900

    Receive & log form data.

--- src/main/java/com/example/jihye/controller/ArticleController.java
+++ src/main/java/com/example/jihye/controller/ArticleController.java
@@ -1,9 +1,13 @@
 package com.example.jihye.controller;
 
+import com.example.jihye.dto.ArticleForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
 
@@ -16,6 +20,12 @@ public class ArticleController {
     @GetMapping("/articles/new") // GET 요청이 해당 url로 오면, 아래 메소드를 수행!
     public String newArticle() {
         return "articles/new"; // 보여줄 뷰 페이지
+    }
+
+    @PostMapping("/articles") // Post 방식으로 "/articles" 요청이 들어오면, 아래 메소드 수행!
+    public String create(ArticleForm form) { // 폼 태그의 데이터가 ArticleForm 객체로 만들어 짐!
+        log.info(form.toString()); // ArticleForm 객체 정보를 확인!
+        return "redirect:/articles"; // 브라우저를 "/articles" url로 보냄!
     }
 
 }
